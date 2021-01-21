bb5c29dad43f6f99165d86b47a3d0f7e584b9fec
carriepresley <carriepresley15@gmail.com>
2021-01-21 11:32:06 -0600

    vid update

--- src/Components/YouTube/YouTubeVid.js
+++ src/Components/YouTube/YouTubeVid.js
@@ -11,7 +11,7 @@ function YouTube2 (){
           className="vid-wrapper"
     
         >
-        <iframe title="youtubevid" width="560" height="315" src="https://www.youtube.com/embed/0poFbaGGDvI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className ="vid-player"></iframe>
+        <iframe title="youtubevid" width="560" height="315" src="https://www.youtube.com/embed/o1B-EG9N490" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className ="vid-player"></iframe>
         </div>
 
     )
